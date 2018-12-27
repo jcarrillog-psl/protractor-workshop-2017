f2cbc52edcef39018911b6c76a814102de9ab319
jcarrillog-psl <jcarrillog@psl.com.co>
2018-12-27 11:24:28 -0500

    improving the first test case

--- protractor.config.js
+++ protractor.config.js
@@ -1,10 +1,10 @@
 exports.config = {
 	framework: 'jasmine' ,
 	seleniumAddress: 'http://localhost:4444/wd/hub' ,
-	specs: [ 'test/spec.js' ],
-	//specs: [ 'test/Google.spec.js'],
+	//specs: [ 'test/spec.js' ],
+	specs: [ 'test/Google.spec.js'],
 	//specs: [ 'test/spec.js','test/Google.spec.js'],
-	//onPrepare: () => {
-	//	browser.ignoreSynchronization = true ;
-	//}
+	onPrepare: () => {
+		browser.ignoreSynchronization = true ;
+	}
 }
