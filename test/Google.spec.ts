1a8979f9eb538dd12343d7f4d124e6c1a551a737
jcarrillog-psl <jcarrillog@psl.com.co>
2018-12-28 16:09:41 -0500

    disable some SELENIUM_PROMISE_MANAGER and using async/await

--- test/Google.spec.ts
+++ test/Google.spec.ts
@@ -4,12 +4,12 @@ export { browser } from 'protractor' ;
 
 describe( 'Given a SDET learning protractor' , () => {
 	describe( 'when open Google Page' , () => {
-		beforeEach(() => {
-			browser.get( 'http://www.google.com' );
+		beforeEach(async() => {
+			await browser.get( 'http://www.google.com' );
 		});
 
-		it( 'then should have a title' , () => {
-			expect(browser.getTitle()).toEqual( 'Google' );
+		it( 'then should have a title' , async () => {
+			await expect(browser.getTitle()).toEqual( 'Google' );
 		});
 	});
 });
