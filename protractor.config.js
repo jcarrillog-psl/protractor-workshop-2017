907fbd021efa3996a9d435ea0f27afbbdc3efa0a
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-01-11 17:07:21 -0500

    Update protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -1,7 +1,7 @@
 exports.config = {
 	framework: 'jasmine' ,
-	seleniumAddress: 'http://localhost:4444/wd/hub' ,
-	//SELENIUM_PROMISE_MANAGER: false,
+	//seleniumAddress: 'http://localhost:4444/wd/hub' ,
+	SELENIUM_PROMISE_MANAGER: false,
 	specs: [ 'test/Google.spec.js'],
 	onPrepare: () => {
 		browser.ignoreSynchronization = true ;
