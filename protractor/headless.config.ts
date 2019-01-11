234ab876982e3611014e6071309474f3183b9c30
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-01-11 17:07:44 -0500

    Update headless.config.ts

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,8 +3,8 @@ import { reporter } from './helpers/reporter' ;
 
 export const config: Config = {
 	framework: 'jasmine' ,
-	seleniumAddress: 'http://localhost:4444/wd/hub' ,
-	//SELENIUM_PROMISE_MANAGER: false,
+	//seleniumAddress: 'http://localhost:4444/wd/hub' ,
+	SELENIUM_PROMISE_MANAGER: false,
 	specs: ['../test/Google.spec.js'],
 	noGlobals: true ,
 	onPrepare: () => {
