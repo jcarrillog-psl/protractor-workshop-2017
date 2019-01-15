76e005b6554c7e76c007a55b8bd1b4dbfad12412
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-01-15 08:25:03 -0500

    Update protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -1,6 +1,5 @@
 exports.config = {
 	framework: 'jasmine' ,
-	//seleniumAddress: 'http://localhost:4444/wd/hub' ,
 	SELENIUM_PROMISE_MANAGER: false,
 	specs: [ 'test/Google.spec.js'],
 	onPrepare: () => {
