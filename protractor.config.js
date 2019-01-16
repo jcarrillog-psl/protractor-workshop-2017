4cf1ed1584281838d1432fcc8e82a68c4ebf50dd
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-01-16 12:51:59 -0500

    Stable version promise async wait chrome headless (#7)
    * SELENIUM_PROMISE_MANAGER: false,//does not work on win10
    
    * Version for UNIX
    the property SELENIUM_PROMISE_MANAGER is activated
    
    * Adding for travis CI
    
    * Update config.ts
    
    * Update headless.config.ts
    
    * Update protractor.config.js
    
    * Update package.json
    
    * Update protractor.config.js
    
    * Update headless.config.ts
    
    * Update config.ts
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update protractor.config.js
    
    * Update headless.config.ts
    
    * Update config.ts
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update package.json
    
    * Update .travis.yml
    
    * Update protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -1,6 +1,6 @@
 exports.config = {
 	framework: 'jasmine' ,
-	seleniumAddress: 'http://localhost:4444/wd/hub' ,
+	SELENIUM_PROMISE_MANAGER: false,
 	specs: [ 'test/Google.spec.js'],
 	onPrepare: () => {
 		browser.ignoreSynchronization = true ;
