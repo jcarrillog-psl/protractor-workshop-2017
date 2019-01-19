060771661b9d9d8f1f8b8741e7f463dd1f0a82e8
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-01-19 16:40:18 -0500

    updated to the same version of branch stable_version_Promise_Async_Wait_Chrome_Headless (#9)
    but configured to OSwin10

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,8 +3,9 @@ import { reporter } from './helpers/reporter' ;
 
 export const config: Config = {
 	framework: 'jasmine' ,
-	SELENIUM_PROMISE_MANAGER: false,
-	specs: ['../test/Google.spec.js'],
+	seleniumAddress: 'http://localhost:4444/wd/hub' ,
+	//SELENIUM_PROMISE_MANAGER: false,//does not work on win10
+	specs: ['../test/Google.spec.js' ],
 	noGlobals: true ,
 	onPrepare: () => {
 		browser.ignoreSynchronization = true ;
