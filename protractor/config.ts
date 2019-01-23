cb81c95f0a8d4dead259292a7cc8219830a895e2
jcarrillog-psl <jcarrillog@psl.com.co>
2019-01-23 17:27:35 -0500

    STATIC CODE ANALYSIS

--- protractor/config.ts
+++ protractor/config.ts
@@ -2,13 +2,13 @@ import { browser, Config } from 'protractor' ;
 import { reporter } from './helpers/reporter' ;
 
 export const config: Config = {
-	framework: 'jasmine' ,
-	seleniumAddress: 'http://localhost:4444/wd/hub' ,
-	//SELENIUM_PROMISE_MANAGER: false,//does not work on win10
-	specs: ['../test/Google.spec.js' ],
-	noGlobals: true ,
-	onPrepare: () => {
-		browser.ignoreSynchronization = true ;
-		reporter();
-	}
-}
+  framework: 'jasmine' ,
+  seleniumAddress: 'http://localhost:4444/wd/hub' ,
+  // SELENIUM_PROMISE_MANAGER: false,//does not work on win10
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true ,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true ;
+    reporter();
+  }
+};
--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,19 +2,19 @@ import { browser, Config } from 'protractor' ;
 import { reporter } from './helpers/reporter' ;
 
 export const config: Config = {
-	framework: 'jasmine' ,
-	seleniumAddress: 'http://localhost:4444/wd/hub' ,
-	//SELENIUM_PROMISE_MANAGER: false,//does not work on win10
-	specs: ['../test/Google.spec.js'],
-	noGlobals: true ,
-	onPrepare: () => {
-		browser.ignoreSynchronization = true ;
-		reporter();
-	},
-	capabilities: {//version headless of chrome
-		browserName: 'chrome' ,
-		chromeOptions: {
-			args: [ '--headless', '--disable-gpu', '--window-size=800,600' ]
-		}
-	}
-}
+  framework: 'jasmine' ,
+  seleniumAddress: 'http://localhost:4444/wd/hub' ,
+  // SELENIUM_PROMISE_MANAGER: false,//does not work on win10
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true ,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true ;
+    reporter();
+  },
+  capabilities: {// version headless of chrome
+    browserName: 'chrome' ,
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  }
+};
