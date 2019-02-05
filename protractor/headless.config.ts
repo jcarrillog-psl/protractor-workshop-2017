b68b0abb4d7aced48dd12e0f1df19223541aee99
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-02-05 08:10:44 -0500

    static code analysis (#10)
    * static code analysis
    
    * some additional white spaces and additional installations
    
    * Update .travis.yml
    
    * Update tslint.json
    
    * Update package.json
    
    * Update .travis.yml

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -2,18 +2,18 @@ import { browser, Config } from 'protractor' ;
 import { reporter } from './helpers/reporter' ;
 
 export const config: Config = {
-	framework: 'jasmine' ,
-	SELENIUM_PROMISE_MANAGER: false,
-	specs: ['../test/Google.spec.js'],
-	noGlobals: true ,
-	onPrepare: () => {
-		browser.ignoreSynchronization = true ;
-		reporter();
-	},
-	capabilities: {//version headless of chrome
-		browserName: 'chrome' ,
-		chromeOptions: {
-			args: [ '--headless', '--disable-gpu', '--window-size=800,600' ]
-		}
-	}
-}
+  framework: 'jasmine' ,
+  SELENIUM_PROMISE_MANAGER: false , // does not work on win10
+  specs: ['../test/Google.spec.js'],
+  noGlobals: true ,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true ;
+    reporter();
+  },
+  capabilities: {// version headless of chrome
+    browserName: 'chrome' ,
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu', '--window-size=800,600']
+    }
+  }
+};
