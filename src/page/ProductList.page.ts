fab2c0ee7149dcb2449a6286de97a294db33c428
jcarrillog-psl <jcarrillog@psl.com.co>
2019-02-19 15:00:56 -0500

    finding how to solve the problem with mouse over

--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,12 +1,27 @@
-import { $, ElementFinder } from 'protractor' ;
+import { $$, ElementFinder, ElementArrayFinder, browser } from 'protractor';
 
 export class ProductListPage {
-    private productList: ElementFinder;
-	
-	construnctor(){
-		this.productList = $('[style*="display: block;"] .button-container > a');
-    }
-    public async goToProductList(): Promise<void> {
-      await this.productList.click();
-    }
+  private products: ElementArrayFinder;
+
+  constructor() {
+    this.products = $$('.product-container');
+  }
+
+  private findByProduct(productName: string): ElementFinder {
+    return this.products
+      .filter((item: ElementFinder) =>
+                item
+                    .$('.product-name')
+                    .getText()
+                    .then((text: string) => text.includes(productName))
+             )
+      .first();
+  }
+
+  public async selectProduct(productName: string): Promise<void> {
+    const card = this.findByProduct(productName);
+
+    await browser.actions().mouseMove(card.$('img')).perform();
+    await card.$('.ajax_add_to_cart_button.btn.btn-default').click();
+  }
 }
