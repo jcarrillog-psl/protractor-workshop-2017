089e553f7a24c58816301758bbbdac5afadd3b26
jcarrillog-psl <jcarrillog@psl.com.co>
2019-02-19 17:02:26 -0500

    rollback

--- src/page/SignOutSession.page.ts
+++ src/page/SignOutSession.page.ts
@@ -1,13 +1,10 @@
-import { $, ElementFinder } from 'protractor' ;
+import { $, ElementFinder, promise } from 'protractor' ;
 
 export class SignOutSessionPage {
-    private signOutSession: ElementFinder;
-      
-	constructour() {
-		this.signOutSession = $( '#header > div:nth-child(2) > div > div > nav > div:nth-child(2) > a' );
+    private get signOutSession(): ElementFinder {
+      return $( '#header > div:nth-child(2) > div > div > nav > div:nth-child(2) > a' );
     }
-	
-    public async goToSignOutSession(): Promise<void> {
-      await this.signOutSession.click();
+    public goToSignOutSession(): promise.Promise<void> {
+      return this.signOutSession.click();
     }
 }
