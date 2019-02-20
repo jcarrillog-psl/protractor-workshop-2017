b1fbae73fb41f1fbc8ec0464952db0a58ce0e33b
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-02-20 10:38:07 -0500

    Update SignOutSession.page.ts

--- src/page/SignOutSession.page.ts
+++ src/page/SignOutSession.page.ts
@@ -1,10 +1,13 @@
-import { $, ElementFinder, promise } from 'protractor' ;
+import { $, ElementFinder } from 'protractor' ;
 
 export class SignOutSessionPage {
-    private get signOutSession(): ElementFinder {
-      return $( '#header > div:nth-child(2) > div > div > nav > div:nth-child(2) > a' );
+    private signOutSession: ElementFinder;
+      
+	constructor() {
+		this.signOutSession = $( '#header > div:nth-child(2) > div > div > nav > div:nth-child(2) > a' );
     }
-    public goToSignOutSession(): promise.Promise<void> {
-      return this.signOutSession.click();
+	
+    public async goToSignOutSession(): Promise<void> {
+      await this.signOutSession.click();
     }
 }
