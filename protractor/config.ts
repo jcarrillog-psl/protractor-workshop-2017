a2b1a8da3f0e1c18ab18172669590bbb9dbfae30
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-04-02 11:08:02 -0500

    JASMINE AWAITS updated (#14)

--- protractor/config.ts
+++ protractor/config.ts
@@ -5,10 +5,14 @@ export const config: Config = {
   framework: 'jasmine' ,
   SELENIUM_PROMISE_MANAGER: false , // does not work on win10
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 3000,
   noGlobals: true ,
   onPrepare: () => {
     browser.ignoreSynchronization = true ;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
   }
 };
