b3ccc96a26eadc4046ad2c24b7eb499d47a74aa6
jcarrillog-psl <jcarrillog@psl.com.co>
2019-04-02 10:58:18 -0500

    JASMINE AWAITS updated

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,10 +5,11 @@ export const config: Config = {
   framework: 'jasmine' ,
   SELENIUM_PROMISE_MANAGER: false , // does not work on win10
   specs: ['../test/**/*.spec.js'],
-  getPageTimeout: 1000,
+  getPageTimeout: 3000,
   noGlobals: true ,
   onPrepare: () => {
     browser.ignoreSynchronization = true ;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   },
   capabilities: {// version headless of chrome
@@ -16,5 +17,8 @@ export const config: Config = {
     chromeOptions: {
       args: ['--headless', '--disable-gpu', '--window-size=800,600']
     }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
   }
 };
