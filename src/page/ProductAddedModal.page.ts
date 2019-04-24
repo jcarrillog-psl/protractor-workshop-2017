9125deb2dcf3673ce5a69845ed48ec90aec9b4d3
jcarrillog-psl <jcarrillog@psl.com.co>
2019-04-24 18:42:20 -0500

    changing locator according to the design of the page, adding explicit time

--- src/page/ProductAddedModal.page.ts
+++ src/page/ProductAddedModal.page.ts
@@ -1,12 +1,16 @@
-import { $, ElementFinder } from 'protractor' ;
+import { browser, $, ElementFinder, ExpectedConditions } from 'protractor' ;
 
 export class ProductAddedModalPage {
+  private EC = ExpectedConditions;
+
   private productAddedModal: ElementFinder;
 
   constructor() {
     this.productAddedModal = $('#add_to_cart > button > span');
   }
+
   public async goToProductAddedModal(): Promise<void> {
+    await browser.wait(this.EC.visibilityOf(this.productAddedModal), 5000);
     await this.productAddedModal.click();
   }
 }
--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -1,12 +1,15 @@
-import { $, ElementFinder } from 'protractor' ;
+import { $, ElementFinder, browser } from 'protractor' ;
 
 export class ProductDetailPage {
   private productDetail: ElementFinder;
+  private moreButton: ElementFinder;
 
   constructor() {
-    this.productDetail = $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
+    this.productDetail = $('.product_img_link');
+    this.moreButton = $('.lnk_view');
   }
   public async goToProductDetail(): Promise<void> {
-    await this.productDetail.click();
+    await browser.actions().mouseMove(this.productDetail).perform();
+    await this.moreButton.click();
   }
 }
