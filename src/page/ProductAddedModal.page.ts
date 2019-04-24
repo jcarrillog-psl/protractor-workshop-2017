e12dffbcf68d1ccb0b81507d4ab16afca66cfe54
jcarrillog-psl <jcarrillog@psl.com.co>
2019-04-24 18:08:35 -0500

    Adding .vscode and explicit waits

--- src/page/ProductAddedModal.page.ts
+++ src/page/ProductAddedModal.page.ts
@@ -1,12 +1,14 @@
-import { $, ElementFinder } from 'protractor' ;
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor' ;
 
 export class ProductAddedModalPage {
   private productAddedModal: ElementFinder;
+  private EC = ExpectedConditions;
 
   constructor() {
     this.productAddedModal = $('#add_to_cart > button > span');
   }
   public async goToProductAddedModal(): Promise<void> {
+    await browser.wait(this.EC.visibilityOf(this.productAddedModal), 5000);
     await this.productAddedModal.click();
   }
 }
--- src/page/ProductList.page.ts
+++ src/page/ProductList.page.ts
@@ -1,12 +1,14 @@
-import { $, ElementFinder } from 'protractor' ;
+import { $, ElementFinder, browser, ExpectedConditions } from 'protractor' ;
 
 export class ProductListPage {
   private productList: ElementFinder;
-  
+  private EC = ExpectedConditions;
+
   constructor() {
     this.productList = $('.button-container > a');
   }
   public async selectProduct(): Promise<void> {
+    await browser.wait(this.EC.visibilityOf(this.productList), 5000);
     await this.productList.click();
   }
 }
