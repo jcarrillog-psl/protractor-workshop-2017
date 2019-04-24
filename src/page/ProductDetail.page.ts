9ecf439f3ca6e0fc1b99a35448d8853aa2de1b32
jcarrillog-psl <jcarrillog@psl.com.co>
2019-04-24 18:46:03 -0500

    changing locator according to the design of the page, adding explicit

--- src/page/ProductDetail.page.ts
+++ src/page/ProductDetail.page.ts
@@ -4,7 +4,8 @@ export class ProductDetailPage {
   private productDetail: ElementFinder;
 
   constructor() {
-    this.productDetail = $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
+    this.productDetail =
+    $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
   }
   public async goToProductDetail(): Promise<void> {
     await this.productDetail.click();
