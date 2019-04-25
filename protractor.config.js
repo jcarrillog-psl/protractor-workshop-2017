e3061468b2a67db7cfdaa4ac173dbd6181d69a41
jcarrillog-psl <45925396+jcarrillog-psl@users.noreply.github.com>
2019-04-25 12:02:03 -0500

    Update protractor.config.js

--- protractor.config.js
+++ protractor.config.js
@@ -2,13 +2,13 @@ exports.config = {
 	framework: 'jasmine' ,
 	SELENIUM_PROMISE_MANAGER: false,
 	specs: [ 'test/Google.spec.js'],
-	onPrepare: () => {
-		browser.ignoreSynchronization = true ;
-	}
-}
-capabilities: {
+  capabilities: {
     browserName: 'chrome',
     chromeOptions: {
       args: ['--no-sandbox']
     },
+  },
+	onPrepare: () => {
+		browser.ignoreSynchronization = true ;
+	}
 }
